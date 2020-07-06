7c9bbfc2129e1f0332ef6ff04083ef72abd7c733
Mariola Wójcik <mwojcik95@gmail.com>
2020-07-06 23:44:05 +0200

    Added request get by id with validation of status code and response body

--- Selenium Webdriver/RestAPITestingBDD/src/test/java/TestCases/TC1_GET_Request.java
+++ Selenium Webdriver/RestAPITestingBDD/src/test/java/TestCases/TC1_GET_Request.java
@@ -3,15 +3,28 @@ package TestCases;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class TC1_GET_Request {
 
-    @Test
+    @Test(priority = 1)
     public void getAllVideoGames(){
         given()
         .when()
                 .get("http://localhost:8080/app/videogames")
         .then()
                 .statusCode(200);
+    }
+
+    @Test(priority = 3)
+    public void getVideoGameById(){
+        given()
+        .when()
+                .get("http://localhost:8080/app/videogames/123")
+        .then()
+                .statusCode(200)
+                .log().body()
+                .body("videoGame.id", equalTo("123"))
+                .body("videoGame.name", equalTo("Skyrim"));
     }
 }
